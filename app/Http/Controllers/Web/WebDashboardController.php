e8fbd776a3f5e61c994ea0a56c40b68b18850576
Gunawan <gsinaga53@gmail.com>
2025-05-01 20:41:18 +0700

    push

--- app/Http/Controllers/Web/WebDashboardController.php
+++ app/Http/Controllers/Web/WebDashboardController.php
@@ -3,8 +3,8 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use App\Models\Matapelajaran;  
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Auth;
 
